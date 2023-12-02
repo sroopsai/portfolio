fdab24bf013a980fda58e911e851ba36dc660722
roopsai <roop.sai.surampudi@proton.me>
2023-12-02 23:24:35 +0530

    Add import components into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Service";
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
+import Blog from "./components/blog/Blog";
+import Testimonials from "./components/testimonials/Testimonials";
+import Pricing from "./components/pricing/Pricing";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
+  );
+}
 
 export default App;
